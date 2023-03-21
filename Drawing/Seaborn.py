07a03e249c7499a3f074022a5b6f18400e3fc4ee
aaalvinnn <2869242945@qq.com>
2023-03-22 00:06:29 +0800

    ADD Heatmap()

--- Drawing/Seaborn.py
+++ Drawing/Seaborn.py
@@ -1,3 +1,8 @@
+'''
+Reference: https://zhuanlan.zhihu.com/p/89353885
+
+'''
+
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
@@ -5,12 +10,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def main():
-    sns.set()   # 图标风格设置，默认为darkgrid风格
+    # 图标风格设置，默认为darkgrid风格，另外四种：whitegrid,dark,white,ticks
+    sns.set()   
     plt.plot()
 
 # 单变量频数直方图
 def UniVariate_Analysis():
-    sns.set()
+    sns.set_style('darkgrid')
     # 生成100个成标准正态分布的随机数
     x = np.random.normal(size=100)
     # 画出频数直方图 kde为核密度估计
@@ -39,9 +45,16 @@ def Hexagonal_Chart():
         sns.jointplot(x=x,y=y,kind='hex',color='k')  #kind='hex'画出六角图
     plt.show()
 
+# 热度图
+def Heatmap():
+    sns.set()
+    uniform_data = np.random.rand(8,8)
+    heatmap = sns.heatmap(uniform_data)
+    plt.show()
 
 if __name__ == '__main__':
     # main()
     # UniVariate_Analysis()
     # Bivariate_Analysis()
-    Hexagonal_Chart()
+    # Hexagonal_Chart()
+    Heatmap()
