5b8907bc48c629504a6a4e25328f9d6eb2955230
aaalvinnn <2869242945@qq.com>
2023-03-21 21:53:14 +0800

    init

---
+++ Drawing/Seaborn.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def main():
+    sns.set()   #图标风格设置，默认为darkgrid风格
+
+
+
+
+if __name__ == '__main__':
+    main()
